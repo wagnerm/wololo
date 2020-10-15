a1f16cb3aa03b23dad3832b806a1bdb592eb3568
Max Wagner <wagnerm@github.com>
2020-10-15 10:18:42 -0600

    It wololos

--- wololo.py
+++ wololo.py
@@ -5,6 +5,7 @@ import traceback
 
 TOKEN = os.getenv("DISCORD_TOKEN")
 BOT_ID = os.getenv("BOT_ID")
+AUDIO_ROOT_PATH = os.getenv("AUDIO_ROOT_PATH")
 
 client = discord.Client()
 
@@ -39,7 +40,6 @@ class BaseCommand(object):
 
     async def handle_message(self, message, message_content):
         if message_content:
-            print(message_content)
             return await self.process_chat(message, message_content)
 
     async def process_chat(self, message, message_content):
@@ -51,18 +51,50 @@ class BaseCommand(object):
                     continue
 
                 func = command["func"]
-                return func(message, message_content)
+                return await func(message, message_content)
 
         return None
 
 
-class Wololo(BaseCommand):
+class DiscordConnectionFailure(Exception):
+    pass
+
+
+class AudioCommand(object):
+
+    async def connect(self, message):
+        voice_channel = message.author.voice.channel
+
+        try:
+            return await voice_channel.connect(reconnect=False)
+        except discord.ClientException:
+            # Already connected, attempt to attain VoiceClient
+            return voice_channel.guild.voice_client
+
+        raise DiscordConnectionFailure("VoiceClient cannot be attained!")
+
+    async def play_in_channel(self, message, audio_source):
+        author_voice = message.author.voice
+        if author_voice == None:
+            print(f"{message.author} not in voice channel, skipping")
+            return
+
+        voice_client = await self.connect(message)
+        if voice_client.is_playing():
+            voice_client.stop()
+
+        voice_client.play(audio_source)
+
+
+class Wololo(BaseCommand, AudioCommand):
     def __init__(self):
         super(Wololo, self).__init__("wololo")
         self.register(None, ".*wololo.*", "Try me", self.wololo)
 
-    def wololo(self, message, message_content):
-        return "WOLOLO"
+    async def wololo(self, message, message_content):
+        audio_source = discord.FFmpegPCMAudio(
+            os.path.join(AUDIO_ROOT_PATH, "wololo/wololo.wav"))
+        return await self.play_in_channel(message, audio_source)
 
 
 class Bot(discord.Client):
